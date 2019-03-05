13910fc169d3aa7fc8a0b3c49939d6acfd85888c
Nicholas Le <nle031@west-mec.org>
2019-03-04 17:31:47 -0700

    Final Commit
    Fixed the problem of the code displaying directly onto the browser, and added a bar graph that helps visualize the percentage of votes each candidate has.
    
    Ready for review.

--- vw_results.js
+++ vw_results.js
@@ -25,29 +25,15 @@
 */
 
 
-
-
-
-/* Callback Function to calculate an array sum */
-function calcSum(value) {
-    totalVotes += value;
-}
-
-/* Function to calculate a percentage */
-function calcPercent(value, sum) {
-    return (100 * value / sum);
-}
-
 // Author JS 
 var reportHTML = "<h1>" + raceTitle + "</h1>";
 
-//For loop that loops through the candidates
+//For loop that runs through the candidates
 for (var i = 0; i <= 7; i++) {
     var totalVotes = 0;
     votes[i].forEach(calcSum);
-    reportHTML += "<table> <caption>" + race[i] + //"</caption>" + "<tr><th>" + "Candidates" + "</th><th>" + "Votes" + "</th></tr>";
-    candidateRows([i], totalVotes);
-    reportHTML += candidateRows;
+    reportHTML += "<table><caption>" + race[i] + "</caption>" + "<tr><th>" + "Candidates" + "</th><th>" + "Votes" + "</th></tr>";
+    reportHTML += candidateRows([i], totalVotes);
     reportHTML += "</table>";
 }
 document.getElementById("section").innerHTML = reportHTML;
@@ -60,7 +46,49 @@ function candidateRows(raceNum, totalVotes) {
         var candidateParty = party[raceNum][j];
         var candidateVotes = votes[raceNum][j];
         var candidatePercent = calcPercent(candidateVotes, totalVotes);
-        rowHTML += "<tr><td>" + candidateName + candidateParty + "</td><td>" + candidateVotes.toLocaleString() + candidatePercent.toFixed(1) + "</td> </tr>";
+
+        //Displays the candidate name and vote numbers
+        rowHTML += "<tr><td>" + candidateName + "(" + candidateParty + ")" + "</td><td>" + candidateVotes.toLocaleString() + "(" + candidatePercent.toFixed(1) + "%)" + "</td>";
+
+        //For loop that runs through and displays the bar
+        for (var k = 0; k < candidatePercent; k++) {
+            rowHTML += createBar(candidateParty, candidatePercent);
+        }
+        rowHTML += "</tr>"
     }
     return rowHTML;
+}
+
+
+/* Callback Function to calculate an array sum */
+function calcSum(value) {
+    totalVotes += value;
+}
+
+/* Function to calculate a percentage */
+function calcPercent(value, sum) {
+    return (100 * value / sum);
+}
+
+//Creates the percentage bar for each candidate using switch cases
+function createBar(partyType) {
+    var barHTML = "";
+    switch (partyType) {
+        case "D":
+            barHTML = "<td class='dem'></td>"
+
+            break;
+
+        case "R":
+            barHTML = "<td class='rep'></td>"
+
+            break;
+
+        case "I":
+            barHTML = "<td class='ind'></td>"
+
+            break;
+
+    }
+    return barHTML;
 }
